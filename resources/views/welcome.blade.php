8c7468479ed3b3fa3ea680fe244e5643eb33de13
shaiful_systema <e0001339@u.nus.edu>
2016-12-12 04:45:06 +0000

    Commit Folder

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome     
+@endsection
+
+@section('content')
+    <div class='row'>
+        <div class="col-6">
+            <h3>Sign Up</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class ="form-control" type="text" name="email" id="email"/>
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your E-Mail</label>
+                    <input class ="form-control" type="text" name="email" id="email"/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class ="form-control" type="password" name="password" id="password"/>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit </button>
+            </form>
+        </div>
+        <div class="col-6">
+            <h3>Sign In</h3>
+            <form action="#" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class ="form-control" type="text" name="email" id="email"/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class ="form-control" type="password" name="password" id="password"/>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit </button>
+            </form>
+        </div>
+        
+    </div>
+
+@endsection
+
+
